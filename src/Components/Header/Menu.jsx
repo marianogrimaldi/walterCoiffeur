4c5d3feb756ccd9165169b67099aab5fe49d6da3
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-11-01 21:49:28 -0300

    primer commit mobile

--- src/Components/Header/Menu.jsx
+++ src/Components/Header/Menu.jsx
@@ -1,4 +1,4 @@
-import menu from "../../assets/img/menu.png"
+import logo from "../../assets/img/LOGO NEGATIVO PNG.png"
 import "./Header.scss"
 import { NavLink } from "react-router-dom"
 
@@ -6,7 +6,7 @@ const Menu = () => {
     return (
         <header>
             <label htmlFor="menu" className="nav__label">
-                    <img src={menu} alt="" className="nav__img" />
+                    <img src={logo} alt="" className="nav__img" />
                 </label>
                 <input type="checkbox" id="menu" className="nav__imput" />
             <ul className="ulFlex">
